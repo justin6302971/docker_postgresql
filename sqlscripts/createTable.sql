13bb17ea3a53ae88d52bcf8e82b3b8529d2e6d45
justin6302971 <justin6302971@hotmail.com>
2022-03-02 22:16:47 +0800

    udpate sql scripts

--- sqlscripts/createTable.sql
+++ sqlscripts/createTable.sql
@@ -1,12 +1,25 @@
-drop table car,person;
+--database
+CREATE DATABASE sampledb
+    WITH 
+    OWNER = tempuser_local
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
 
-create table person(
+--create extension
+CREATE EXTENSION "uuid-ossp"
+
+--schema
+CREATE SCHEMA sp
+AUTHORIZATION tempuser_local;
+
+-- drop table car,person;
+CREATE TABLE IF NOT EXISTS sp.person(
 	id uuid DEFAULT uuid_generate_v4() not null PRIMARY KEY ,
 	first_name varchar(50) not null,
 	last_name varchar(50) not null
 );
 
-create table car(
+CREATE TABLE IF NOT EXISTS sp.car(
  	id bigserial not null primary key,
 	name varchar(50) not null,
 	price numeric(19,2) not null,
@@ -15,9 +28,23 @@ create table car(
 );
 
 
+insert into sp.car(name,price) values ('land rover','20000'),('gmc','30000.5');
+insert into sp.person(first_name,last_name) values ('justin','chien'),('hengi','chien');
+update sp.car set person_id =(select id from sp.person  where first_name like '%justin%' limit 1) where name like '%land%' 
 
-insert into car(name,price) values ('land rover','20000'),('gmc','30000.5');
-insert into person(first_name,last_name) values ('justin','chien'),('hengi','chien');
+
+select * from sp.car;
+select * from sp.person ;
+
+select c.*,p.* from sp.car as c left join sp.person as p on c.person_id=p.id where p.id is null
+select c.*,p.* from sp.car as c inner join sp.person as p on c.person_id=p.id
+select c.*,p.* from sp.car as c left join sp.person as p on c.person_id=p.id
+select c.*,p.* from sp.car as c full outer join sp.person as p on c.person_id=p.id
+
+-- select * from sp.car where name like '%land%'
+-- select id from sp.person  where first_name like '%justin%' limit 1
+
+
 -- insert into person(first_name,last_name,car_id) values ('justin1','chien1',1),('hengi2','chien2',2);
 --  insert into person(first_name,last_name,car_id) values ('justin4','chien4',4);
 -- select * from car;
@@ -27,13 +54,13 @@ insert into person(first_name,last_name) values ('justin','chien'),('hengi','chi
 --create table with uuid(uuid-ossp extension)
 -- SELECT uuid_generate_v4();
 
-CREATE TABLE contacts (
-    contact_id uuid DEFAULT uuid_generate_v4() not null PRIMARY KEY ,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
-    phone VARCHAR
-);
+-- CREATE TABLE contacts (
+--     contact_id uuid DEFAULT uuid_generate_v4() not null PRIMARY KEY ,
+--     first_name VARCHAR NOT NULL,
+--     last_name VARCHAR NOT NULL,
+--     email VARCHAR NOT NULL,
+--     phone VARCHAR
+-- );
 
 -- INSERT INTO contacts (
 --     first_name,
